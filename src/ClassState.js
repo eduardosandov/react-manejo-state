60a25bfcd4afadf003b5ef802bf143d1e1844569
Eduardo Sandoval <eduardosandoval.atc@gmail.com>
2022-11-15 23:01:46 -0600

    ReactState

---
+++ src/ClassState.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class ClassState extends React.Component {
+    render () {
+        return (
+            <div>
+                <h2>Eliminar ClassState</h2>
+
+                <p>Por favor, escribe el codigo de seguridad</p>
+            </div>
+        )
+    }
+}
+
+export { ClassState };
